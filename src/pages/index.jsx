0fcecf16fa16184f656f0fb4d989a503b7ac6cdc
Jkose7 <ojosema10@gmail.com>
2024-03-07 21:53:01 -0500

    change colors

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,25 +2,25 @@ import { Aside_news } from "../components/Aside_news"
 
 export function Index() {
     return (
-        <section className=" mx-28 xl:mx-80 gap-7 flex flex-col">
-            <nav className="bg-transparent flex justify-between font-titulos items-center text-primary-color">
+        <section className="mx-28 xl:mx-80 gap-7 flex flex-col">
+            <nav className="bg-transparent flex justify-between font-titulos items-center text-primary-color h-14">
                 <div className="text-3xl">
                     MeowMelody
                 </div>
 
-                <div className="hover:bg-second-color hover:text-white p-3 rounded-sm transition-all duration-100">
-                    <button className="text-xl">Create</button>
+                <div className="hover:bg-primary-color hover:text-black rounded-sm transition-all duration-150">
+                    <button className="text-xl px-3 py-1">Create</button>
                 </div>
             </nav>
 
-            <section className="h-full flex gap-5 flex-col">
+            <section className="h-full flex gap-1 flex-col">
                 <div className="bg-slate-600 flex justify-center h-96 ">
                     <article >
                         Noticia 1
                     </article>
                 </div>
 
-                <div className="flex ">
+                <div className="flex gap-1">
                     <Aside_news/>
                     <Aside_news/>
                     <Aside_news/>
