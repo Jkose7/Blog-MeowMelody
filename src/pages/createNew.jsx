68dcc156316c55f1c70fcd0fb1f9e107d7374c9b
NicoNico04 <nicollcanocastro465@gmail.com>
2024-03-07 22:24:36 -0500

    Create route for new news

---
+++ src/pages/createNew.jsx
@@ -0,0 +1,8 @@
+
+export function CreateNews () {
+    return(
+        <section className="bg-emerald-800">
+            <h1 className="text-white">Form pa mi novio hemosho</h1>
+        </section>
+    )
+}
