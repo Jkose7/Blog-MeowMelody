a87e1ee1b106922bee3fa30f13d65495cf686c39
Jkose7 <ojosema10@gmail.com>
2024-03-10 20:15:42 -0500

    prop validations

--- src/components/mainNew.jsx
+++ src/components/mainNew.jsx
@@ -1,7 +1,13 @@
+import Proptypes from 'prop-types'
+
 export function MainNew ({children}){
     return (
         <div className="bg-second-color dark:bg-primary-color h-full flex justify-center items-center rounded-sm">
             {children}
         </div>
     )
+}
+
+MainNew.propTypes = {
+    children:Proptypes.string,
 }
