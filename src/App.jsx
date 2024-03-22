9e3afdc9c5c927a98fdc3a753dd77e40ee28e38a
Jkose7 <ojosema10@gmail.com>
2024-03-22 10:12:30 -0500

    creacion del componente NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,32 @@ import { Nav } from "./components/nav";
 import { Footer } from "./components/footer";
 
 import { ThemeProviter } from "./providers/ThemeProviter.jsx";
+import { NotFound } from "./pages/NotFound.jsx";
 
 
 function App() {
   return (
-    <ThemeProviter>
-      <main className="bg-primary-color dark:bg-second-color -z-10 h-full w-full absolute flex flex-col justify-center py-6 px-5 md:px-16 lg:px-28 xl:px-48 2xl:px-80">
-        <Nav />
+    <>
 
-        <Routes>
-          <Route path="/" element={<Index ></Index>}></Route>
-          <Route
-            path="/createNew"
-            element={<CreateNews></CreateNews>}
-          ></Route>
-          
-        </Routes>
+      <ThemeProviter>
+        <main className="bg-primary-color dark:bg-second-color -z-10 h-full w-full absolute flex flex-col justify-center py-6 px-5 md:px-16 lg:px-28 xl:px-48 2xl:px-80">
+          <Nav />
 
-        <Footer />
-      </main>
-    </ThemeProviter>
+          <Routes>
+            <Route path="/" element={<Index ></Index>}></Route>
+            <Route
+              path="/createNew"
+              element={<CreateNews></CreateNews>}
+            ></Route>
+            <Route path="*" element={<NotFound></NotFound>} />
+          </Routes>
+
+
+          <Footer />
+        </main>
+      </ThemeProviter>
+
+    </>
   );
 }
 
