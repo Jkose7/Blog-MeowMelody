d290c742ba1c74a6c6c8576692eba21bd1132dc4
NicoNico04 <nicollcanocastro465@gmail.com>
2024-03-29 13:33:53 -0500

    letra tema oscuro

--- src/pages/createNew.jsx
+++ src/pages/createNew.jsx
@@ -17,7 +17,7 @@ export function CreateNews() {
   } = useForm();
 
   return (
-    <section className=" bg-primary-color dark:bg-second-color dark:bg-blend-color-dodge h-[80vh]">
+    <section className=" bg-primary-color dark:bg-second-color dark:bg-blend-color-dodge h-[80vh] dark:text-primary-color">
       <form 
       id="myform"
       action="" 
