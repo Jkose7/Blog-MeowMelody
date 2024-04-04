6a49740269605e04602aed36872ec58cd9692a4c
NicoNico04 <nicollcanocastro465@gmail.com>
2024-04-04 00:08:47 -0500

    responsive modal

--- src/components/modalNews.jsx
+++ src/components/modalNews.jsx
@@ -11,16 +11,22 @@ export function ModalNews({cerrar}) {
 
   return (
     <div className="modal text-white dark:text-black ">
-      <div className="modal-info dark:bg-primary-color">
-        <h1 className="text text-center font-titulos text-4xl">La noticia se creo de manera correcta</h1>
+      <div className="modal-info dark:bg-primary-color minicel:h-[80%] minicel:w-[88%] celinter:h-[40%] sm:h-[40%] lg:w-[70%]">
+        <h1 className="text text-center font-titulos text-sm minicel:text-lg celinter:text-2xl sm:text-3xl lg:text-4xl">La noticia se creo de manera correcta</h1>
         <div className="buttons ">
-          <Link className="home text-center border-primary-color dark:text-secborder-second-color dark:border-second-color dark:hover:bg-second-color dark:hover:text-white" to="/">
+          <Link className="home text-center text-sm sm:text-xl minicel:p-1 celinter:p-2   
+          border-primary-color dark:border-second-color dark:text-second-color 
+          dark:hover:bg-second-color dark:hover:text-white" to="/">
             Inicio
           </Link>
-          <Link to={`/news/${currentID}`} className="news text-center border-primary-color dark:text-secborder-second-color dark:border-second-color dark:hover:bg-second-color 
+          <Link to={`/news/${currentID}`} className="news text-center text-sm sm:text-xl  minicel:p-1 celinter:p-2
+          border-primary-color dark:text-second-color dark:border-second-color 
+          dark:hover:bg-second-color dark:hover:text-white
           ">Ver noticia
           </Link>
-          <button className="newnews border-primary-color dark:text-secborder-second-color dark:border-second-color dark:hover:bg-second-color dark:hover:text-white" onClick={cerrar}>Crear noticia</button>
+          <button className="newnews text-sm sm:text-xl minicel:p-1 celinter:p-2 
+          border-primary-color dark:text-second-color dark:border-second-color 
+          dark:hover:bg-second-color dark:hover:text-white" onClick={cerrar}>Crear noticia</button>
         </div>
       </div>
     </div>
